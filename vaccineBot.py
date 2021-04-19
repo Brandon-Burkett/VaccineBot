403802d4605ad2faa22619c47e1dd52563e50351
Brandon Burkett <brandonburkett01@gmail.com>
2021-04-19 11:20:53 -0400

    Added fully booked message

--- vaccineBot.py
+++ vaccineBot.py
@@ -27,8 +27,11 @@ async def vaccineCheck():
             if item['status'] != 'Fully Booked':
                 await channel.send('@everyone')
                 await channel.send(item['city'] + ": " + item['status'])
+            else:
+                await channel.send("All locations Fully Booked")
             # await channel.send(item['city'] + ": " + item['status'])
             print(item['city'] + ": " + item['status'])
+
 
 @client.event
 async def on_ready():
